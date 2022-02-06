b3f1dc9e1e66707f383a96e9d3e5b9fa6e9085ea
PikaKight <tmmarcus73@gmail.com>
2022-02-06 11:25:18 -0500

    getting something working

--- src/calendar.js
+++ src/calendar.js
@@ -1,2 +1,6 @@
 var CLIENT_ID = '987812981681-79vatf5sdq307g4kahg0kqs3rins2ee2.apps.googleusercontent.com';
 var API_KEY = 'AIzaSyCYBI4gs3xu4nFtWDIqsEDX4Qwc4aN0ecY'
+
+var gcal = require('google-calendar');
+
+var google_calendar =
---
+++ src/popup.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var checkButton = document.getElementById('home');
+    checkButton.addEventListener('click', function() {
+     alert("Hey your button is working!");
+    }, false);
+  }, false);
